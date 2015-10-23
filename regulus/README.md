bceeac19c60532c991b7c35ea2e6f69f11c75b65
Martin Fischer <fischer.carlito@gmail.com>
2015-10-23 19:23:54 +0200

    added examples files for CDC

---
+++ regulus/README.md
@@ -0,0 +1,1 @@
+Regulus port for micropython
---
+++ regulus/main.h
@@ -0,0 +1,17 @@
+#ifndef __MAIN_H
+#define __MAIN_H
+
+#include "stm32l1xx.h"
+
+#include "usbd_core.h"
+#include "usbd_desc.h"
+#include "usbd_cdc.h"
+#include "usbd_cdc_if.h"
+
+#include "usb_ll.h"
+
+// #include "uart.h"
+
+#include "stdio.h"
+
+#endif // __MAIN_H
