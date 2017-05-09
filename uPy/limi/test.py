35cfe1db1171ab7105aa5c3b56654d83069c68d3
Martin Fischer <fischer.carlito@gmail.com>
2017-05-09 21:23:09 +0200

    uPy/jupyter: add initial docs

--- uPy/limi/test.py
+++ uPy/limi/test.py
@@ -2,23 +2,67 @@ import pyb, machine
 import limi
 from writer import Writer
 
+import utime
 import myfont
+
+
+WIDTH = 160
+HEIGHT = 128
+
+NR_X = 10
+NR_Y = 10
+
+print("TEST")
+WIDTH_RX= WIDTH // NR_X
+HEIGHT_RY= HEIGHT // NR_Y
 
 limi.powerup()
 display = limi.init_oled()
 
-w = Writer(display.framebuf,myfont,width=160,height=128)
-w.printstring("HEEYY")
-# 
-# display.pixel(1,1,1)
-# display.pixel(140,1,1)
-# display.pixel(140,127,1)
-# def color_oled(r,g,b):
+print("HHH")
+# w = Writer(display.framebuf,myfont,width=WIDTH,height=HEIGHT)
+# w.printstring("HEEYY")
+
+import gc
+gc.collect() 
+
+# start = utime.ticks_ms()
+# print("starting: {}".format(start))
+while(1):
+        
+    for i in range(0,100):
+        for x in range(0,NR_X):
+            for y in range(0,NR_Y):
+                display.framebuf.fill_rect(
+                    x* WIDTH_RX,
+                    y* HEIGHT_RY,
+                    WIDTH_RX - 3 ,
+                    HEIGHT_RY - 3,
+                    2+x*2+y*15+i*10)
+                    # display.fill_rect(60,1,160,100,r << 3 )
+                    # display.fill_rect(60,1,160,100,r << 8)
+                    # w.printstring(str(r)+"\n")
+                display.show()
+                # pyb.delay(10)
+                    
+start = utime.ticks_ms()
+print("starting: {}".format(start))
+
+# for x in range(0,100):
+#     display.framebuf.fill_rect(0,0,WIDTH, HEIGHT,0)
+#     display.show()
+#     display.framebuf.fill_rect(0,0,WIDTH, HEIGHT,7)
+#     display.show()
+    # pyb.delay(10)
     
-for r in range(0,255):
-    display.framebuf.fill_rect(60,1,160,100, r << 13 )
-    # display.fill_rect(60,1,160,100,r << 3 )
-    # display.fill_rect(60,1,160,100,r << 8)
-    w.printstring(str(r)+"\n")
+    
+for i in range(0,16):
+    display.framebuf.scroll(10,0)
     display.show()
-    pyb.delay(100)
+    
+for i in range(0,12):
+    display.framebuf.scroll(0,10)
+    display.show()
+    
+print("finished in: {}".format(utime.ticks_ms()-start))
+    
